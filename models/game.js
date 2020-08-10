cd815b4a4a111d36b64de8540977276c66dddee1
smb2796 <sean.brown2796@gmail.com>
2020-08-09 22:28:20 -0400

    updating user schema

--- models/game.js
+++ models/game.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const gameSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
     players: { 
         player1: { type: String, required: true },
         player2: { type: String, required: true }
--- models/user.js
+++ models/user.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    rating: { type: Number, required: true},
-    profilePicture: { type: String, required: true}
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true , minlength: 6 },
+    profilePicture: { type: String, required: true},
+    games: { type: String, required: true},
+    name: { type: String, required: true },
+    birthdate: { type: String, required: true },
+    rating: { type: Number, required: true}
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
