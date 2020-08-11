9f2acdd6f947409d011e9ab5600fe73ad1bb30e0
smb2796 <sean.brown2796@gmail.com>
2020-08-10 20:45:47 -0400

    adding square class

---
+++ models/square.js
@@ -0,0 +1,8 @@
+export default class Square {
+    constructor (color, inhabitant, xPos, yPos) {
+        this.color = color;
+        this.inhabitant = inhabitant;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
