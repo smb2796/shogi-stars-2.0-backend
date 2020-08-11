d3adee671138dfad2750498a58bf3e8b1de230da
smb2796 <sean.brown2796@gmail.com>
2020-08-10 21:05:41 -0400

    adding file upload handling with multer

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const userController = require('../controllers/user-controller');
+const fileUpload = require('../middleware/file-upload');
 const router = express.Router();
 
 router.get('/all', userController.getUsers);
@@ -9,6 +10,7 @@ router.get('/all', userController.getUsers);
 
 router.post(
     '/signup',
+    fileUpload.single('image'),
     [
     check('name')
         .not()
